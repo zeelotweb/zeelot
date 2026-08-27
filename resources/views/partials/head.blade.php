b568364696768917d0e7cfcb68b57ae01d16b644
zeelotweb <67663419+zeelotweb@users.noreply.github.com>
2026-08-27 00:35:49 -0400

    fat off

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,16 +4,11 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="icon" href="/favicon.png" type="image/png">
+<link rel="apple-touch-icon" href="/favicon.png">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
-
-<link rel="stylesheet" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
-<script src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js"></script>
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
